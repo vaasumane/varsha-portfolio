caed85b21ecf6c57a5904a7544dc658ab14792bf
= <=>
2026-07-02 23:30:17 +0530

    fixes1

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,7 @@
 import React,{useRef, useState} from "react";
 import emailjs from '@emailjs/browser';
 // import '../EmailForm.css';
-// import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const Contact = () => {
   const form = useRef();
